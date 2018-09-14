7b1f3cbb5a31d5f223c2740faa9c2288925f9659
zouchanglin <15291418231@163.com>
2018-09-14 10:20:20 +0800

    修改！

---
+++ 32_CLionTest/cmake-build-debug/CMakeFiles/32_CLionTest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/32_CLionTest.dir/main.c.obj"
+  "32_CLionTest.pdb"
+  "32_CLionTest.exe"
+  "32_CLionTest.exe.manifest"
+  "lib32_CLionTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/32_CLionTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
