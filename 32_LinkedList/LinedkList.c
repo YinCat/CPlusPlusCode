f0957b1d58bc735e05d47078d99b6734590a92aa
zouchanglin <15291418231@163.com>
2018-09-14 21:15:18 +0800

    Add List func!

--- 32_LinkedList/LinedkList.c
+++ 32_LinkedList/LinedkList.c
@@ -10,12 +10,59 @@ void InitList(pNodeList *pplist)
     *pplist = NULL;
 }
 
-void Destory(pNodeList *pplist);
-
-void PushBack(pNodeList *pplist,DataType data)
+void Destory(pNodeList *pplist)
 {
+    pNodeList list = *pplist;
+    pNodeList cur = NULL;
+    assert(pplist);
+    if(*pplist == NULL)
+        return;
+    while
 
 }
 
-void PrintList(pNodeList plist);
+static pNodeList BuyNode(DataType data)
+{
+    pNodeList newNode = NULL;
+    newNode = (pNodeList)malloc(sizeof(ListNode));
+    if(newNode == NULL)
+    {
+        perror("BuyNode::malloc");
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+void PushBack(pNodeList *pplist,DataType data)
+{
+    pNodeList cur = NULL;
+    assert(pplist);
+    if(*pplist == NULL)
+    {
+        *pplist = BuyNode(data);
+    }
+    else
+    {
+        cur = *pplist;
+        while(cur->next != NULL)
+        {
+            cur = cur->next;
+        }
+        cur->next = BuyNode(data);
+    }
+}
+
+void PrintList(pNodeList plist)
+{
+    pNodeList cur = NULL;
+    if(plist == NULL)
+        return;
+    while(cur)
+    {
+        printf("%d--->",cur->data);
+        cur = cur->next;
+    }
+    printf("END\n");
+}
 
